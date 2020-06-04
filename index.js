cf10656197b774afc2263db1bd4f58fcca51e6aa
Stuepp <arthurh.c@hotmail.com>
2020-06-04 19:12:39 -0300

    Add all my files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*const products = document.querySelectorAll('.product')
+
+    (function shuffle() {
+        products.forEach(product => {
+            let randomPos = Math.floor(Math.random() * 4)
+            product.style.order = randomPos
+        })
+    })();
+*/
+function randomPos() {
+    let Imgnum = Math.floor(Math.random() * 4)
+}
